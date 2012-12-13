2343761e4dba5105d8704e5df90e17dd234dd67a
Kőszegi Gábor <info@kgab0r.net>
2012-12-13 20:56:11 +0100

    first up

---
+++ src/net/kgab0r/skate/eventListener.java
@@ -0,0 +1,56 @@
+package net.kgab0r.skate;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class eventListener implements Listener{
+	ItemStack boots;
+	private skate plug;
+	Location from, to;
+	public static Material down,skate;
+	public static String ws;
+	public eventListener(skate plugin){
+		this.plug = plugin;
+		plug.getServer().getPluginManager().registerEvents(this, plugin);
+	}
+
+	@EventHandler
+	public void onPlayerMove(PlayerMoveEvent evt) {
+		Player player = evt.getPlayer();
+		if(player.hasPermission("iceskate.skate")) {
+		Location loc = player.getLocation();
+		loc.setY(loc.getY()-1);
+		Block b = loc.getBlock();
+		
+		loc.setY(loc.getY()-2);
+		Block b2 = loc.getBlock();
+		Material mat2 = b2.getType(); 
+		
+		boots = player.getInventory().getBoots();
+		Material btype = Material.AIR;
+		if(boots!=null){
+			btype = boots.getType();
+		}else{
+			player.setWalkSpeed((float) 0.2);
+		}
+		Material mat = b.getType();
+
+		if(btype==skate){
+			if(mat==Material.AIR && mat2 == down){
+				player.setWalkSpeed(Float.parseFloat(ws)); //BUG
+			}else if(mat==down){
+				player.setWalkSpeed(Float.parseFloat(ws));
+			}
+			else{
+				player.setWalkSpeed((float) 0.2);
+			}
+		}
+		}}
+	
+}
---
+++ src/net/kgab0r/skate/skate.java
@@ -0,0 +1,71 @@
+package net.kgab0r.skate;
+import java.io.IOException;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+public final class skate extends JavaPlugin {
+	
+	public void onEnable(){
+		getLogger().info("IceSkate enabled!");
+		try {
+		    Metrics metrics = new Metrics(this);
+		    metrics.start();
+		} catch (IOException e) {
+		    // Failed to submit the stats :-(
+		}
+		new eventListener(this);
+		
+		reloadcfg();
+		
+	}
+ 
+	private void reloadcfg() {
+		getConfig().options().copyDefaults(true);
+		saveConfig();
+		if(getConfig().getBoolean("enabled")){
+			 eventListener.ws = getConfig().getString("speed");
+			 getLogger().info("Speed set to: "+eventListener.ws);
+			// goon
+			Material downblock = Material.getMaterial(getConfig().getInt("downblock"));
+			Material skate = Material.getMaterial(getConfig().getInt("skate"));
+			
+			//passtoel
+			eventListener.down = downblock;
+			eventListener.skate = skate;
+			
+			//echo
+			getLogger().info("DownBlock set to: "+downblock);
+			getLogger().info("Skate set to: "+skate);
+			
+			 
+}else{
+	this.setEnabled(false);
+}
+	}
+
+	public void onDisable(){
+		getLogger().info("IceSkate disabled!");
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+		if (!sender.isOp())
+			return true;
+		final String commandStr = cmd.getName().toLowerCase();
+		if(commandStr.equalsIgnoreCase("is")){
+			if (args.length != 1){
+				return false;}
+			else if(args[0].equalsIgnoreCase("reload")){
+				this.reloadConfig();
+				reloadcfg();
+				sender.getServer().broadcastMessage("[Skate] reloaded!");
+				return true;
+			}
+			sender.getServer().broadcastMessage("[Skate] Command not found!");
+			return false;
+		}
+		return true; 
+	}
+}
